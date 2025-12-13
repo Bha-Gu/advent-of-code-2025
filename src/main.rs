b0e6e27994517b6ca4b605f1ef30402259f71f3e
bha-gu <bhaumik050402@gmail.com>
2025-12-13 19:08:05 +0530

    day 1 and 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod day2;
+use day2::{EX_INPUT1 as INPUT, p2};
+// use day1::{INPUT1 as INPUT, p1_1, p1_2};
+
 fn main() {
-    println!("Hello, world!");
+    let out = p2(INPUT);
+    println!("{out}");
 }
